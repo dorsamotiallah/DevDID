a89eb148b72e9c3ce1c4340f0ca74a141a0037b2
Ali <krm98.ali98@gmail.com>
2022-01-23 17:19:13 +0330

    API completed

--- backend/App.js
+++ backend/App.js
@@ -5,6 +5,7 @@ var Web3 = require('web3');
 var web3 = new Web3("HTTP://127.0.0.1:7545");
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
 // Retrieve ABI & Contract address from Smart contract JSON
 var Verification_json = Verification_json = require("D:/Education/University/TERM7/Narm1/Project/Development/Project/DevDID/backend/build/contracts/Verification.json");
@@ -12,18 +13,16 @@ var contract_address = Verification_json['networks']['5777']['address'];
 var abi = Verification_json['abi'];
 // Connecting to smart contract
 var verf_contract = new web3.eth.Contract(abi,contract_address);
-console.log("\nabi: ",abi);
+// console.log("\nabi: ",abi);
 
-async function accounts (){
+async function accounts () {
 //setting role's accounts
   var accounts = await web3.eth.getAccounts();
   var issuer_account = accounts[0];
   var holder_account = accounts[1];
   var verifier_account = accounts[2];
 
-  return new Promise(resolve => {
-    resolve([issuer_account,holder_account,verifier_account]);
-  });
+  return [issuer_account,holder_account,verifier_account];
 }
 
 //holders storage
@@ -37,7 +36,7 @@ const signTest = async function(msg,account){
   //Hashing the input message 
   let hashedmessage = await verf_contract.methods.getHash(msg).call();
 
-  //console.log("hashnsg : ",hashedmessage);
+  console.log("hashnsg : ",hashedmessage);
 
   // Hashing again with prefix added to last hash 
   let prefixed_hashed_msg = await verf_contract.methods.getEthSignedHash(hashedmessage).call();
@@ -64,7 +63,7 @@ const signTest = async function(msg,account){
 // Retrieving the sender of the message 
 const verif = async function (input) {
 
-  // console.log("Verif Input : " , input);
+  console.log("Verif Input : " , input);
   var signature = input[1];
   // Splitting signature
   var r = signature.slice(0, 66);
@@ -90,7 +89,7 @@ const verif = async function (input) {
 async function issuer (raw_vc,account){
   // Combine raw_vc and account to create a signature for later VC
   let sign_res = await signTest(raw_vc,account);//[prefixed_hashed_msg,signature]
-  //console.log("hashedmsg and signiture: ",sign_res);
+  console.log("hashedmsg and signiture: ",sign_res);
   
   var issued_vc = {}; // It is supposed to be result of Issuer Role
   issued_vc["issuer public_key"] = account; 
@@ -112,7 +111,7 @@ async function holder (issued_vc,account){
   // Combine issued_vc and account to create a signature for current VC
   let sign_res = await signTest(issued_vc,account);//[prefixed_hashed_msg,signature]
 
-  //console.log("hashedmsg and signiture: ",sign_res);
+  console.log("hashedmsg and signiture: ",sign_res);
 
   var holder_vc = {};// It is supposed to be result of Holder Role
   holder_vc["issuer public_key"] = issued_vc["issuer public_key"];
@@ -143,13 +142,22 @@ async function verifier(holder_vc){
   var holder_public_key = await verif([holder_vc["holder prefixed hashed vc"],holder_vc["holder signature"]]);
 
 
-  if(issuer_public_key === holder_vc["issuer public_key"]){
+  if (issuer_public_key === holder_vc["issuer public_key"]){
     console.log("\nissuer successfully verified");
+
+    if (holder_public_key === holder_vc["holder public_key"]){
+      console.log("\nholder successfully verified");
+      return "Valid VC";
+
+    }
+
+    return "Valid Issuer , Invalid Holder";
+    
   }
 
-  if(holder_public_key === holder_vc["holder public_key"]){
-    console.log("\nholder successfully verified");
-  }
+  return "Invalid Issuer , Invalid Holder";
+
+  
 
 }
 
@@ -184,15 +192,53 @@ async function main(){
 }
 
 main()
-*/
 
-app.post('/pure_meta_data',async function(req,res){
-  var accs = await accounts();
-  var meta_data = JSON.parse(req.body);
+
+*/
+app.use(bodyParser.json());
+
+
+app.post('/sign_by_issuer',async function(req,res){
+  let accs = await accounts();
+
+  console.log("Accs :" , accs);
+  console.log("Body : ",req.body);
+  var meta_data = req.body;
   var issuer_vc = await issuer(meta_data,accs[0]);
-  var holder_vc = await holder (issuer_vc,accs[1]);
-  vcs.push(holder_vc);
+  res.json(issuer_vc);
+  res.end();
+
 })
+
+app.post('/sign_by_holder',async function(req,res){
+  let accs = await accounts();
+  
+  console.log("Accs :" , accs);
+  console.log("Body : ",req.body);
+  var issuer_vc = req.body;
+
+  var holder_vc = await holder (issuer_vc,accs[1]);
+
+  res.json(holder_vc);
+  res.end();
+
+})
+
+app.post('/verification',async function(req,res){
+  let accs = await accounts();
+  
+  console.log("Accs :" , accs);
+  console.log("Body : ",req.body);
+  var holder_vc = req.body;
+  // console.log("Body : ",meta_data);
+  let result = await verifier(holder_vc);
+  res.json(result);
+  res.end();
+
+})
+
+
+
 
 var server = app.listen(8081, function () {
   var host = server.address().address
